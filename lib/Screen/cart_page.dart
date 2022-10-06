00de4fc236ab2194d09c3347485e152abf7e0db1
chee_tech <91067088+Chee-tech@users.noreply.github.com>
2022-10-06 10:19:38 +0100

    yes to all

--- lib/Screen/cart_page.dart
+++ lib/Screen/cart_page.dart
@@ -10,6 +10,7 @@ class CartPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<CartPage> {
+  final List<String> _myList = List.generate(6, (index) => 'Product $index');
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -17,53 +18,240 @@ class _ShopPageState extends State<CartPage> {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Container(
-        height: height,
-        width: width,
-        color: Colors.white,
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 50),
-                const Text(
-                  "Find Products",
-                  style: TextStyle(
-                    fontFamily: "Gilroy-ExtraBold",
-                    fontStyle: FontStyle.normal,
-                    fontSize: 20,
-                  ),
+          height: height,
+          width: width,
+          color: Colors.white,
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+              title: const Text(
+                "My Cart",
+                style: TextStyle(
+                  fontFamily: "Gilroy-ExtraBold",
+                  fontStyle: FontStyle.normal,
+                  fontSize: 24,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(CupertinoIcons.location_solid),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      "Location",
-                      style: TextStyle(
-                        fontFamily: "Gilroy",
-                        fontStyle: FontStyle.normal,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+              ),
+            ),
+            body: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {},
+                        child: ListView.separated(
+                          scrollDirection: Axis.vertical,
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return MyPadding(
+                              _myList[index],
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return const Divider(
+                              color: Colors.black,
+                            );
+                          },
+                          itemCount: _myList.length,
+                        ),
                       ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        //Go to Checkout button....
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                width: 411,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: const Color(0xff53B175),
+                                ),
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      const Text(
+                                        'Go to Checkout',
+                                        style: TextStyle(
+                                          fontFamily: "Gilroy",
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Container(
+                                        height: 20,
+                                        width: 80,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey.withOpacity(0.2),
+                                        ),
+                                        child: const Text(
+                                          "total price",
+                                          style: TextStyle(
+                                            fontFamily: "Gilroy-Light",
+                                            fontStyle: FontStyle.normal,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class MyPadding extends StatelessWidget {
+  const MyPadding(String myList, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          height: 180,
+          width: 620,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.black26,
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.white12,
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                          image: AssetImage("assets/images/pepper.png"),
+                          fit: BoxFit.contain)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Name of Product",
+                      style: TextStyle(
+                        fontFamily: "Gilroy-ExtraBold",
+                        fontStyle: FontStyle.normal,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        //Minus
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                          child: const Icon(CupertinoIcons.minus),
+                        ),
+
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 6),
+                          child: const Text(
+                            "01",
+                            style: TextStyle(
+                              fontFamily: "Gilroy-ExtraBold",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+
+                        //Plus
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                          child: const Icon(CupertinoIcons.plus),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 20,
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(Icons.cancel),
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          child: const Text(
+                            "Price",
+                            style: TextStyle(
+                              fontFamily: "Gilroy-ExtraBold",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                const Text(
-                  "Cart Page",
-                  style: TextStyle(fontSize: 50),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
